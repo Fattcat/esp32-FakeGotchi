d9cd7cab98468a8cf6b2f105f921efee286d15a2
Fattcat - but not fat cat <109801079+Fattcat@users.noreply.github.com>
2025-05-29 17:59:17 +0200

    Create FakeGotchiV2.ino

---
+++ FakeGotchiV2.ino
@@ -0,0 +1,7 @@
+// Code for Double Attack - need to use 2 esp32 boards
+/*
+   ---------------------------------------------------------------------
+   --        Code will be awailable if this Repo gets 100 stars       --
+   -- Then will be contacted 5 radom github accounts for sending code --
+   ---------------------------------------------------------------------
+*/
